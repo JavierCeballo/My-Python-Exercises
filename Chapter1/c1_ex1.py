d32593d78c9fb98d06d9fd144f3efeca81ef3f28
Javier Ceballo <jvrceballo@gmail.com>
2022-07-16 18:45:55 -0400

    Add the solution to exercises 1 and 2

---
+++ Chapter1/c1_ex1.py
@@ -0,0 +1,14 @@
+# Exercise 1: Mainling Address
+#
+# Create a program that displays your name and complete
+# mainling address formated in the manner that you would
+# usually see it on the outside of an envelope. Your program
+# does not need to read any input from the user.
+
+
+# Solution:
+#
+# Displays my name and complete mainling address
+print("Javier Ceballo")
+print("Calle Tibisay, nro. 25-A")
+print("Barcelona, Anzoátegui, 6001")
---
+++ Chapter1/c1_ex2.py
@@ -0,0 +1,15 @@
+# Exercise 2: Hello
+#
+# Write a program that asks the user to enter
+# his or her name. The program should respond
+# with a message that says hello to the user,
+# usig his or her name.
+
+
+# Solution
+
+# Ask the user his or her name
+username = input("Enter your name, please: ")
+
+# Displays a message with the user name
+print(f"Hello, {username}.")
